5c9ecc93204476fecec7cfa3adf3d8c439033836
EdKenjiF5116 <edukenji2003@gmail.com>
2024-04-18 19:52:24 -0300

    01

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,6 @@
-console.log("calcular...")
-
-// manipulação do DOM
-
-// let = local
-// var = global
-// const = constante
-
-
-
-document.querySelector("#Nível").addEventListener("change", function() {
-    const nivel = document.querySelector("#Nível").value;
-    document.querySelector("#Lvl").innerText = nivel;
-    calcular(); // Chama a função calcular para atualizar o orçamento, se necessário
+var rangeInput = document.querySelector('.Nível_linha7 input[type="range"]');
+var spanLvl = document.getElementById('Lvl');
+rangeInput.addEventListener('input', function() {
+       spanLvl.textContent = rangeInput.value;
 });
+spanLvl.textContent = rangeInput.value;
